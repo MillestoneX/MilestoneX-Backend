2f13489fac2575159f9b357fe79d95586cb6185b
LaGodxy <algodicino@gmail.com>
2026-03-24 15:18:03 -0700

    Add comprehensive analytics functionality

--- src/projects/projects.module.ts
+++ src/projects/projects.module.ts
@@ -1,16 +1,41 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { MulterModule } from '@nestjs/platform-express';
 import { Project } from './entities/project.entity';
 import { Donation } from './entities/donation.entity';
+import { ProjectHistory } from './entities/project-history.entity';
+import { ProjectImage } from './entities/project-image.entity';
 import { User } from '../users/entities/user.entity';
 import { ProjectsController } from './projects.controller';
 import { ProjectsService } from './providers/projects.service';
+import { ImageUploadService } from './services/image-upload.service';
+import { FileUploadService } from '../common/services/file-upload.service';
 import { SearchService } from './services/search.service';
+import { AnalyticsService } from './services/analytics.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Project, Donation, User])],
+  imports: [
+    TypeOrmModule.forFeature([Project, Donation, ProjectHistory, ProjectImage, User]),
+    MulterModule.register({
+      limits: {
+        fileSize: 5 * 1024 * 1024, // 5MB
+        files: 10, // Max 10 files per request
+      },
+    }),
+  ],
   controllers: [ProjectsController],
-  providers: [ProjectsService, SearchService],
-  exports: [ProjectsService, SearchService],
+  providers: [
+    ProjectsService, 
+    ImageUploadService, 
+    FileUploadService, 
+    SearchService, 
+    AnalyticsService
+  ],
+  exports: [
+    ProjectsService, 
+    ImageUploadService, 
+    SearchService, 
+    AnalyticsService
+  ],
 })
 export class ProjectsModule {}
